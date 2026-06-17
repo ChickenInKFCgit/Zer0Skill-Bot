9ee86c39849b166bea7237b101e30839123b6845
Ewen <linktriforce2611@gmail.com>
2026-06-17 23:48:13 +0200

    tentative d'ajout de description

--- bot/bot_commands.py
+++ bot/bot_commands.py
@@ -1,6 +1,8 @@
 """
 Définit toutes les commandes du bot et l'initialise.
 """
+import discord
+
 import bot_intents as boti
 from discord.ext import commands
 
@@ -13,6 +15,20 @@ get_bot = lambda : bot
 
 #Commandes
 
-@bot.command()
-async def test(ctx,arg):
-    await ctx.send(arg)
+@bot.tree.command(name="test", description="Une commande de test qui répète ton message")
+async def test(interaction: discord.Interaction, arg:str):
+    await interaction.response.send_message(arg)
+
+
+@bot.event
+async def on_ready():
+    """
+    Tente de syncroniser les descriptions des commandes avec les commandes, et affiche une erreur si échec.
+    Cette fonction s'éxecute automatiquement lorsque le bot se connecte.
+    """
+    try:
+        await bot.tree.sync()
+        print("syncronisation réalisée")
+    except Exception as e:
+        print("Erreur lors de la syncronisation : "+e)
+    
