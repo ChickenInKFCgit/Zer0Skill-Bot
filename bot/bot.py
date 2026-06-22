c58c17432924a65f440683a09f6d64e7d6d67226
Ewen <linktriforce2611@gmail.com>
2026-06-22 16:19:04 +0200

    Entry point restructuré

--- bot/bot.py
+++ bot/bot.py
@@ -1,21 +1,23 @@
 """
 Lance le bot discord. Est l'entry point de Zer0.
 """
-import bot_commands as botc
-import bot_intents as boti
+import discord 
 from discord.ext import commands
 
-
 #chargement du token
-with open(file="token.secret", mode="r") as f:
-    TOKEN = f.read()
+with open(file="token.secret", mode="r") as f: TOKEN = f.read()
 
 
-# Récupération du bot
-#initialisation du bot et récupération de ses intentions
+# Récupération des intentions du bot
+import bot_intents as boti
 intents = boti.get_intents()
+
+#initialisation du bot 
+
 client = commands.Bot(command_prefix='/', intents=intents) 
 
+# Chargement des commandes et des évènements du bot
+import bot_commands as botc
 botc.load_commands(client)
 
 # Chargement du bot
