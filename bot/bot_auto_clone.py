c9741bb1b3fbf6c897ef5cb5b247da4b6f22a4e2
Ewen <ewenmenez2007@gmail.com>
2026-06-22 18:55:59 +0200

    fonctionnalités de cloning

--- bot/bot_auto_clone.py
+++ bot/bot_auto_clone.py
@@ -7,5 +7,29 @@ si module non trouvé : pip install gitpython
 
 from git import Repo 
 
-def cloner(git_url:str, repo_dir:str):
-    Repo.clone_from(git_url, repo_dir)
+def clone_service(servicename:str) -> bool:
+    """
+    Clone un service.
+    Renvoie False si échec du clone du repo ou si repo introuvable dans repositories.tsv, True si tout a fonctionné.
+    """
+    if (servicename in d_repos.keys()):
+        urlrepo = d_repos[servicename]
+        pathrepo = "services"
+        return __cloner(git_url=urlrepo, repo_dir=pathrepo)
+    else:
+        return False
+
+def __cloner(git_url:str, repo_dir:str) -> bool:
+    rep = Repo.clone_from(git_url, repo_dir)
+    return rep != None
+
+def load_repositories():
+    f = open("services/repositories.tsv",mode="r",encoding="utf-8")
+
+    d={}
+    for ligne in f.read().split("\n"):
+        nom_repo, url_repo = ligne.split("\t");
+        d[nom_repo] = url_repo
+    return d
+
+d_repos = load_repositories()
