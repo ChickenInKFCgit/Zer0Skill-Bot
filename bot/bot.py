e1068d4e3164378aad89d2c44bdb010e462f30d7
Ewen <linktriforce2611@gmail.com>
2026-06-22 16:16:41 +0200

    rectification de la structure v1

--- bot/bot.py
+++ bot/bot.py
@@ -2,6 +2,8 @@
 Lance le bot discord. Est l'entry point de Zer0.
 """
 import bot_commands as botc
+import bot_intents as boti
+from discord.ext import commands
 
 
 #chargement du token
@@ -10,7 +12,11 @@ with open(file="token.secret", mode="r") as f:
 
 
 # Récupération du bot
-client = botc.get_bot()
+#initialisation du bot et récupération de ses intentions
+intents = boti.get_intents()
+client = commands.Bot(command_prefix='/', intents=intents) 
+
+botc.load_commands(client)
 
 # Chargement du bot
 client.run(token=TOKEN)
--- bot/bot_commands.py
+++ bot/bot_commands.py
@@ -3,24 +3,20 @@ Définit toutes les commandes du bot et l'initialise.
 """
 import discord
 
-import bot_intents as boti
-from discord.ext import commands
 
-#initialisation du bot et récupération de ses intentions
-intents = boti.get_intents()
-bot = commands.Bot(command_prefix='/', intents=intents) # Note : self.intents a été corrigé en intents ici pour que le code puisse s'exécuter
 
-#getter du bot
-get_bot = lambda : bot
+
 
 #Commandes
 
-@bot.tree.command(name="test", description="Une commande de test qui répète ton message")
-async def test(interaction: discord.Interaction, arg:str):
-    await interaction.response.send_message(arg)
+def load_commands(bot):
+    @bot.tree.command(name="repete", description="Une commande de test qui répète ton message")
+    async def repete(interaction: discord.Interaction, arg:str):
+        await interaction.response.send_message(arg)
 
 
-@bot.event
-async def on_ready():
-    await bot.tree.sync()
+    @bot.event
+    async def on_ready():
+        await bot.tree.sync()
+
     
